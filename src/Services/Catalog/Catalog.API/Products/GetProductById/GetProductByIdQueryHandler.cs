945390989e193c7e73164bb06f40db384b2edfb8
anuragbeesolver <142773555+anuragbeesolver@users.noreply.github.com>
2024-08-22 17:36:31 +0530

    Catalog API> GetProductById, GetProductByCategory, DeleteProduct
    Catalog API> GetProductById, GetProductByCategory, DeleteProduct

---
+++ src/Services/Catalog/Catalog.API/Products/GetProductById/GetProductByIdQueryHandler.cs
@@ -0,0 +1,21 @@
+﻿namespace Catalog.API.Products.GetProductById
+{
+    public record GetProductByIdQuery(Guid id):IQuery<GetProductByIdResult>;
+
+    public record GetProductByIdResult(Product Product);
+
+    internal class GetProductByIdQueryHandler(IDocumentSession session, ILogger<GetProductByIdQueryHandler> logger)
+        : IQueryHandler<GetProductByIdQuery, GetProductByIdResult>
+    {
+        public async Task<GetProductByIdResult> Handle(GetProductByIdQuery query, CancellationToken cancellationToken)
+        {
+            var result = await session.LoadAsync<Product>(query.id);
+
+            if (result == null)
+            {
+                throw new ProductNotFoundException();
+            }
+            return new GetProductByIdResult(result);
+        }
+    }
+}
